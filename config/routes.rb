af18decb813afd86f5bdda714e0455b2d2568178
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-04-18 07:07:39 +0000

    topページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'homes/top'
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/edit'
+  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
