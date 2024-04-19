b3fda141e271c8d57fb3dc57a2f41f0442105d4c
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-04-19 05:04:13 +0000

    レイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,5 @@
 class BooksController < ApplicationController
-  def new
-   
-  end 
+
   
   def create
     book = Book.new(book_params)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
