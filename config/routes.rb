3a17dfb49dd59ef2a49e41e012f8f50c5f2780b2
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-04-21 13:01:12 +0000

    [finish]課題２

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as:'destroy_book'
   get '/top' => 'homes#top'
   root to: 'books#top'
+  resources :books
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
